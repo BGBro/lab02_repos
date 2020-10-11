fb842a3c7a25a30d55718f7bf474b61a01aedb11
BGBro <bohdan.horbachov.itis.2020@lpnu.ua>
2020-10-11 12:55:48 +0300

    first master commit

---
+++ 2.2/Lab_02.cpp
@@ -0,0 +1,23 @@
+﻿// Lab_02.cpp
+// Горбачов Богдан
+// Лабораторна робота № 2.
+// Лінійні програми.
+// Варіант 3
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+	double a;
+	double z1;
+	// double z2;
+	cout << "a = "; cin >> a;
+	z1 = (sin(2 * a) + sin(5 * a) - sin(3 * a)) / (cos(a) + 1 - 2 * pow(sin(2 * a), 2));
+	// z2 = 2 * sin(a);
+	cout << endl;
+	cout << "z1 = " << z1 << endl;
+	// cout << "z2 = " << z2 << endl;
+}
